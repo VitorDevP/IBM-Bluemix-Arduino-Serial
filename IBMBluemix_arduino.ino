0f1abd92350a705315b6eaffcd648b15dd17d493
Vitor Rodrigues <vitorhenriquerodrigues@gmail.com>
2017-01-01 20:02:10 -0200

    Add files via upload

---
+++ IBMBluemix_arduino.ino
@@ -0,0 +1,31 @@
+//int t;
+
+void setup() {
+  Serial.begin(9600);   //Start serial comunication
+}
+
+void loop() {  
+  Temp();  
+  Serial.flush();  //Clean Serial flow
+  delay(10000);
+}
+
+
+int Temp(){
+  unsigned int wadc;
+  int t;
+  
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);
+  
+  delay(100);
+
+  ADCSRA |= _BV(ADSC); 
+
+   while (bit_is_set(ADCSRA,ADSC)){}
+    wadc = ADCW;
+    t = (wadc - 351.31 ) / 1.22; 
+  
+    Serial.write(t);
+}
+
